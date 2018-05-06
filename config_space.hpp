b80b1509ae15510f334254fcfb53ee4e25a12004
TheLethalCode <kousshikraj.raj@gmail.com>
2018-05-06 12:29:48 +0530

    Reaching target, certain errors to be Resolved

--- config_space.hpp
+++
@@ -1,8 +0,0 @@
-#include<bits/stdc++.h> 
-#include<opencv2/opencv.hpp>
-
-using namespace std;
-using namespace cv;
-
-void config_space(Mat src_img,int bot_height,int bot_length,Mat config[]);
-int IsValid(Mat src_img,int bot_height,int bot_length,Point p,int theta);
--- obstacle_distance.cpp
+++ obstacle_distance.cpp
@@ -7,9 +7,9 @@ int isValid(int i,int j,int r,int c) {
 	return (i >= 0 && j >= 0 && i < r && j <c);
 }
 
-vector<vector<double> > BFS(Mat img) {
-	vector<vector<double> > distance(img.rows,vector<double>(img.cols,0));
-	vector<vector<int> > visited(img.rows,vector<int>(img.cols,0));
+vector< vector<double> > BFS(Mat img) 
+{
+	vector< vector<double> > distance(img.rows,vector<double>(img.cols,FLT_MAX));
 
 	queue<Point> q;
 	Mat visualise = img.clone();
@@ -20,34 +20,42 @@ vector<vector<double> > BFS(Mat img) {
 	findContours(img, contours, heirarchy, CV_RETR_TREE, CV_CHAIN_APPROX_NONE);
 
 	//push boundary
-	for(int i = 0;i<contours.size();i++) {
-		for(int j = 0;j<contours[i].size();j++) {
+	for(int i = 0;i<contours.size();i++)
+	{
+		vector< vector<bool> > visited(img.rows,vector<bool>(img.cols,false));
+		for(int j = 0;j<contours[i].size();j++) 
+		{
 			q.push(contours[i][j]);
-			visited[contours[i][j].y][contours[i][j].x] = 1;
+			visited[contours[i][j].y][contours[i][j].x] = true;
 			distance[contours[i][j].y][contours[i][j].x] = 0.0;
 		}
-	}
 
-	while(!q.empty()) {
-		Point v = q.front();
-		q.pop();
 
-		for(int i = v.y-1;i<=v.y+1;i++) {
-			for(int j = v.x-1;j <= v.x+1;j++) {
-				if(isValid(i,j,img.rows,img.cols)) {
-					if(visited[i][j] == 0 && visualise.at<uchar>(i,j) < 200) {
-						//
-						visualise.at<uchar>(i,j) = 150;
-						//
-						q.push(Point(j,i));
-						visited[i][j] = 1;
-						distance[i][j] = distance[v.y][v.x] + 1.0;
+		while(!q.empty()) 
+		{
+			Point v = q.front();
+			q.pop();
+
+			for(int i = v.y-1;i<=v.y+1;i++) 
+			{
+				for(int j = v.x-1;j <= v.x+1;j++) 
+				{
+					if(isValid(i,j,img.rows,img.cols)) 
+					{
+						if(!visited[i][j]) 
+						{
+							if(visualise.at<uchar>(i,j) > 100)
+								distance[i][j]=0;
+							else if(distance[i][j] > distance[v.y][v.x] + 1.0)
+								distance[i][j] = distance[v.y][v.x] + 1.0;
+							q.push(Point(j,i));
+							visited[i][j] = 1;
+						}
 					}
 				}
 			}
+				
 		}
-			
 	}
-
 	return distance;
 }
